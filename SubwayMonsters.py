9e27c2bd73981e9f160dfe9ec92d9fb4ef7f9762
NovemberJuliett <137638315+NovemberJuliett@users.noreply.github.com>
2023-07-13 19:54:54 +0300

    Create SubwayMonsters.py

---
+++ SubwayMonsters.py
@@ -0,0 +1,69 @@
+from faker import Faker
+fake = Faker("ru_RU")
+
+import random
+
+import file_operations
+
+SKILLS = ["Стремительный прыжок", "Электрический выстрел", "Ледяной удар", "Стремительный удар", "Кислотный взгляд", "Тайный побег", "Ледяной выстрел", "Огненный заряд"]
+SKILLS_VOCABULARY = {
+    'а': 'а͠', 
+    'б': 'б̋', 
+    'в': 'в͒͠',
+    'г': 'г͒͠', 
+    'д': 'д̋', 
+    'е': 'е͠',
+    'ё': 'ё͒͠', 
+    'ж': 'ж͒', 
+    'з': 'з̋̋͠',
+    'и': 'и', 
+    'й': 'й͒͠', 
+    'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+}
+def main():
+    if __name__ == '__main__':
+        main()  
+runic_skills = [] 
+for name in SKILLS:
+    for key in SKILLS_VOCABULARY.keys():
+            rune = SKILLS_VOCABULARY[key]
+            name = name.replace(key, rune)
+            runic_skills.append(name)
+
+for i in range(10):
+    context = {"first_name": fake.first_name(),
+   "last_name": fake.last_name(), 
+   "city": fake.city(),
+   "job": fake.job(),
+   "strength": random.randint(3, 18),
+   "agility": random.randint(3, 18),
+   "endurance": random.randint(3, 18),
+   "intelligence": random.randint(3, 18),
+   "luck": random.randint(3, 18),
+   "skills": random.sample(runic_skills, 3)}
+    file_name = 'result{}.txt'.format(i)
+    file_operations.render_template("template.txt", "Cards/new_result{}.txt".format(i), context)
+
+   
+
+
+
